b2ad4dd20f4a95fa6951863d6d4739f39ec2dcba
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-09-10 19:23:10 -0700

    impl

---
+++ index.js
@@ -0,0 +1,39 @@
+var through = require('through2');
+
+var writable = {writableObjectMode: 1};
+var objectMode = {objectMode: 1};
+
+
+function addProps(key, val){
+  return makeStream(objectMode, key, val);
+}
+
+
+addProps.stringify = function(key, val){
+  return makeStream(writable, key, val);
+};
+
+
+function makeStream(obj, key, val){
+  var mixinObj;
+
+  if(!val && typeof key === 'object') mixinObj = key;
+
+  through(obj, function(chunk, enc, cb){
+    chunk = JSON.parse(JSON.stringify(chunk));
+
+    if(mixinObj){
+      Object.keys(mixinObj).forEach(function(key){
+        chunk[key] = mixinObj[key];
+      });
+    }else{
+      chunk[key] = val;
+    }
+
+    if(obj === writable) chunk = JSON.stringify(chunk);
+
+    return cb(null, chunk);
+  });
+}
+
+module.exports = addProps;
