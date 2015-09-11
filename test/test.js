4d599ee1dc9a158bb14183419a29a5b1bd52b762
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-09-10 22:32:52 -0700

    practical test structure

--- test/test.js
+++ test/test.js
@@ -11,3 +11,21 @@ test('Makes streams', function(t){
   t.ok(addProps('a', 'b')._readableState.objectMode, 'Normal output is in readableObjectMode');
   t.notOk(addProps.stringify('a', 'b')._readableState.objectMode, 'stringified output is not in readableObjectMode');
 });
+
+
+test('Adds properly', function(t){
+  t.plan(1);
+
+  var obj = {a: 'b'};
+
+  var cases = [
+    {fn: addProps, args: {key: null, val: null}, expected: obj}]
+
+  cases.forEach(function(props){
+    var trans = props.fn(props.args.key, props.args.val);
+    trans.end(obj)
+    trans.on('data', function(d){
+    t.deepEqual(d, props.expected, 'No-op with empty props');
+    });
+  });
+})
